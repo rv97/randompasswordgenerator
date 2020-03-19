e8b95b0af69b373940dc54f2ef261e440448d99e
Vignesh R <aengines@gmail.com>
2020-03-20 00:53:32 +0530

    Used string class to get the letters, digits and punc

--- rand_gen.py
+++ rand_gen.py
@@ -3,17 +3,18 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox
 import pyperclip
+import string
 
 
-EASY = "abcdefghijklmnopqrstuvwxyz"
-MED = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-STRONG = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#@$%&*()+,-./:;<=>?[]\^_|"
+WEAK = string.ascii_lowercase
+MED = string.ascii_letters+string.digits
+STRONG = MED+string.punctuation
 
 def generate_password(len, type):
     password = ""
     if type == 0:
         for i in range(len):
-            password = password + random.choice(EASY)
+            password = password + random.choice(WEAK)
         return password
 
     elif type == 1:
@@ -47,7 +48,7 @@ length_combo['values'] = (8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26
 length_combo.current(0)
 length_combo.grid(row=0, column=1)
 
-radio_low = Radiobutton(window, variable=radio_var, text="Low", value=0)
+radio_low = Radiobutton(window, variable=radio_var, text="Weak", value=0)
 radio_low.grid(row=0, column=2)
 radio_med = Radiobutton(window, variable=radio_var, text="Medium", value=1)
 radio_med.grid(row=0, column=3)
